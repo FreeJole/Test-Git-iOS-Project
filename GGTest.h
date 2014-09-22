cdb81102833940f7eb1f813af6808d8a9dc276b5
Garrett Gosch <garrettgosch@Lindamarias-iMac.local>
2014-09-22 11:05:13 -0400

    added a test class

---
+++ GGTest.h
@@ -0,0 +1,13 @@
+//
+//  GGTest.h
+//  Git Test
+//
+//  Created by Garrett Gosch on 9/22/14.
+//  Copyright (c) 2014 MyStuff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGTest : NSObject
+
+@end
---
+++ GGTest.m
@@ -0,0 +1,13 @@
+//
+//  GGTest.m
+//  Git Test
+//
+//  Created by Garrett Gosch on 9/22/14.
+//  Copyright (c) 2014 MyStuff. All rights reserved.
+//
+
+#import "GGTest.h"
+
+@implementation GGTest
+
+@end
